a9db814f72bc753d4a0b21794fffba3e0e87d247
bellicose100xp <chandan7@icloud.com>
2015-03-10 19:56:49 -0700

    work_update

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+let numbers = [1,2,3,4,5];
+
+let sum = 0;
+
+numbers.forEach(n => sum += n);
+
+let double = numbers.map(n => n * 2);
+
+console.log(numbers);
+console.log(sum);
+console.log(double);
 
