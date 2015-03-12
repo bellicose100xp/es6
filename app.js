8cba42eb58e5fd3e7841b4c1c19921389536ed16
bellicose <bellicose.agnostic@gmail.com>
2015-03-12 08:47:07 -0700

    hmoe_update

--- app.js
+++ app.js
@@ -13,20 +13,49 @@ let copyTemplate = () => {
 document.querySelector('#copy-button').addEventListener('click', copyTemplate, false);
 
 let clearCopies = () => {
-  let p = document.querySelectorAll('p');
+    let p = document.querySelectorAll('p');
+    console.log(`type of p is ${Object.prototype.toString.call(p)}`);
+    for (let i = 0; i < p.length; i++) {
 
-    for(let i=0; i< p.length; i++) {
-
-    p[i].parentNode.removeChild(p[i]);
+        p[i].parentNode.removeChild(p[i]);
 
     }
-
-    let x = [1,2,4,5,6];
-
-    x.forEach((num) => console.log(num) );
-
-    console.log(`type of x is ${Object.prototype.toString.call(x)} & type of p is ${Object.prototype.toString.call(p)}`);
-
 };
 
 document.querySelector('#clear-copies').addEventListener('click', clearCopies, false);
+
+let elementProto = Object.create(HTMLElement.prototype);
+
+elementProto.createdCallback = function () {
+    this.innerHTML = '<h1>Comment Section</h1><textarea></textarea><br><button type="submit">Submit</button>';
+};
+
+let element = document.registerElement('my-element', {
+    prototype: elementProto
+});
+
+//let e1 = new element();
+let e1 = document.createElement('my-element');
+console.log(Object.prototype.toString.call(e1));
+document.body.appendChild(e1);
+
+let button_proto = Object.create(HTMLButtonElement.prototype);
+console.log(Object.prototype.toString.call(button_proto));
+button_proto.createdCallback = function () {
+    this.innerHTML = "Extension";
+    this.value = "ex value";
+    this.style.color = "red";
+};
+
+let myButton = document.registerElement('my-button',{
+    prototype: button_proto,
+    extends : 'button'
+});
+
+let myButton1 = document.createElement('button','my-button');
+let clearCopyElement = document.querySelector('#clear-copies');
+clearCopyElement.parentNode.insertBefore(myButton1, clearCopyElement.nextSibling);
+
+
+
+
